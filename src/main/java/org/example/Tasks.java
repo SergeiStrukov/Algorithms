b3d155606615a15883eadf5c31b30de5cd127c6a
SergeiStrukov <serega.strukov21@gmail.com>
2024-05-16 15:00:30 +0400

    Решили задачку по алгоритмам

---
+++ src/main/java/org/example/Tasks.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tasks {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(0, 1, 5, 6, 8);
+        System.out.println(sortArray(list));
+    }
+
+    public static List<Integer> sortArray(List<Integer> list) {
+        List<Integer> newList = new ArrayList<>();
+        int count = 0;
+        for (Integer i : list) {
+            if (i == 0) {
+                count++;
+            } else {
+                newList.add(i);
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            newList.add(0);
+        }
+        return newList;
+    }
+}
